d51c6cb6cd353bcb4755314eb15c85df9cb10bc5
Franfran1612 <franfransilva160@gmail.com>
2025-11-14 13:28:58 -0300

    varios ajustes

--- app.py
+++ app.py
@@ -1,58 +1,16 @@
 from flask import Flask, request, jsonify, flash, url_for, render_template, redirect, session
 from select import select
 
-from routes import post_usuario, post_login, post_voluntario, post_ongs, post_animal
+from routes import post_usuario, post_login, post_ongs, post_animal, get_animais, get_usuarios, post_voluntario, \
+    get_ongs, get_voluntario
 
 app = Flask(__name__)
 app.secret_key = "sua_chave_secreta"
 @app.route('/')
 def index():
-    return render_template('Cadastro.html')
+    flash("entrada, aplicacao", category="success")
+    return render_template('Pagina_inicial.html')
 
-# ---------------------- LOGIN ----------------------
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        # Obter os dados necessarios para enviar para a API
-        cpf = request.form['cpf']
-        senha = request.form['senha']
-
-        # envio para a api
-        resultado = post_login(cpf, senha)
-
-        # verificar se deu sucesso
-        if resultado.status_code == 200:
-            # salvar na session id e nome do usuario
-            session['id_usuario'] = resultado.json()['id_usuario']
-            session['usuario_nome'] = resultado.json()['nome']
-            flash('Login bem-sucedido!', 'success')
-            return redirect(url_for('pagina_inicial'))
-        else:
-            flash('Usuario senha inválidos.', 'danger')
-            return redirect(url_for('login'))
-
-    return render_template('Pagina_login.html')
-
-#  ---------------------- CADASTRAR USUÁRIO ----------------------
-@app.route('/usuario/cadastrar', methods=['GET', 'POST'])
-def cadastrar_usuario():
-    if request.method == 'POST':
-        nome = request.form.get('nome')
-        telefone = request.form.get('telefone')
-        email = request.form.get('email')
-        senha = request.form.get('senha')
-        cpf = request.form.get('cpf')
-        # enviar os dados para a API
-        resultado = post_usuario(nome, telefone, email, senha, cpf)
-
-        # verificar se deu sucesso
-        if resultado == 201:
-            flash('cadastro bem-sucedido!', 'success')
-        else:
-            flash('erro no cadastro.', 'danger')
-            return redirect(url_for('cadastrar_usuario'))
-
-    return render_template('Cadastro.html')
 
 #  ---------------------- PAGÍNA INICIAL ----------------------
 @app.route('/pagina_inicial', methods=['GET'])
@@ -67,31 +25,9 @@ def pagina_doacoes():
 def doacoes_realizada():
     return render_template('adoacoes_realizada.html')
 
-@app.route('/voluntario/cadastrar', methods=['GET', 'POST'])
-def cadastrar_voluntario():
-    if request.method == 'POST':
-        nome = request.form.get('nome')
-        telefone = request.form.get('telefone')
-        data_nascimento = request.form.get('data_nascimento')
-        email = request.form.get('email')
-        senha = request.form.get('senha')
-        cpf = request.form.get('cpf')
-
-        # enviar os dados para a API
-        resultado = post_voluntario(nome, telefone,data_nascimento, email, senha, cpf)
-
-        # verificar se deu sucesso
-        if resultado.status_code == 200:
-            flash('Login bem-sucedido!', 'success')
-        else:
-            flash('erro usuario.', 'danger')
-            return redirect(url_for('cadastrar_voluntario'))
-
-    return render_template('cadastro_voluntario.html')
-
 @app.route('/voluntario', methods=['GET'])
 def voluntario():
-    return render_template('Pagina_Voluntario.html')
+    return render_template('pagina_voluntario.html')
 
 @app.route('/resgate', methods=['GET'])
 def resgate():
@@ -100,44 +36,196 @@ def resgate():
 # Rota para adicionar a ONG
 @app.route('/ongs', methods=['GET'])
 def ongs():
-    return render_template('ongs.html')
+    return render_template('casdastro_ong.html')
 
 @app.route('/pagamento', methods=['GET'])
 def pagamento():
     return render_template('pagamento_pix.html')
 
-# ----------------------PAGINA ADOTAR --------------------------
-@app.route('/pagina_adotar', methods=['GET'])
-def pagina_adotar():
-    return render_template('Pagina_Adotar.html')
+# ---------------------- LOGIN ----------------------
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Obter os dados necessarios para enviar para a API
+        cpf = request.form['cpf']
+        senha = request.form['senha']
 
-# ----------------------INFORMAÇÃO DE ADOÇÃO--------------------------
+        # envio para a api
+        resultado = post_login(cpf, senha)
+
+        print(resultado.status_code)
+        # verificar se deu sucesso
+        if resultado.status_code == 200:
+            # salvar na session id e nome do usuario
+            session['id_usuario'] = resultado.json()['usuario']
+            flash('Login bem-sucedido!', 'success')
+            return redirect(url_for('pagina_inicial'))
+        else:
+            flash('Usuario senha inválidos.', 'danger')
+            return redirect(url_for('login'))
+
+    return render_template('pagina_login.html')
+
+
+# listar usuario
+@app.route('/usuarios', methods=['GET', 'POST'])
+def listar_usuarios():
+
+    usuarios = get_usuarios()
+
+    print(usuarios)
+    return render_template("tabela_usuario.html", usuarios=usuarios ['usuarios'])
+
+# ---------------------- CADASTRAR USUARIO ----------------------
+@app.route('/usuario/cadastrar', methods=['GET', 'POST'])
+def cadastro_usuario():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        cpf = request.form.get('cpf')
+        telefone = request.form.get('telefone')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+
+        resultado = post_usuario(nome, cpf, telefone, email, senha)
+
+        if resultado == 201:
+            flash('Cadastro bem-sucedido!', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash(f'erro: {resultado}', 'danger')
+            return redirect(url_for('cadastro_usuario'))
+    return render_template('cadastro_usuario.html')
+
+
+# informações dos ongs
 @app.route('/informacao', methods=['GET'])
 def informacao():
     return render_template('informacao.html')
 
-@app.route('/animais', methods=['GET'])
-def animais():
-    return render_template('Pagina_Adotar.html')
+# listar animal/ adotar animal
+@app.route('/animais')
+def listar_animais():
 
+    animais = get_animais()
 
-# ---------------------- CADASTRAR ANIMAL ----------------------
+    print(animais)
+    return render_template("pagina_adotar.html", animais=animais ['animais'])
+
+# cadastrar animal
 @app.route('/animal/cadastrar', methods=['GET', 'POST'])
 def cadastro_animal():
     if request.method == 'POST':
-        nome_animal = request.form.get('nome_animal')
+        categoria = request.form.get('categoria')
+        nome = request.form.get('nome')
         raca = request.form.get('raca')
         idade = request.form.get('idade')
+        sexo = request.form.get('sexo')
+        imagem = request.form.get('imagem')
 
-        resultado = post_animal(nome_animal, raca, idade)
+        resultado = post_animal(categoria, nome, raca, idade, sexo, imagem)
 
-        if resultado.status_code == 200:
+        if resultado == 201:
             flash('Login bem-sucedido!', 'success')
-            return redirect(url_for('pagina_adotar'))
+            return redirect(url_for('listar_animais'))
         else:
             flash('erro em cadastrar animal .', 'danger')
             return redirect(url_for('cadastro_animal'))
     return render_template('cadastro_animal.html')
+
+# listar voluntario
+@app.route('/voluntario')
+def listar_voluntario():
+
+    voluntario = get_voluntario()
+
+    print(voluntario)
+    return render_template("tabela_usuario.html", voluntario=voluntario ['voluntario'])
+
+
+# cadastro voluntario
+@app.route('/cadastro_voluntario', methods=['GET', 'POST'])
+def cadastro_voluntario():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        cpf = request.form.get('cpf')
+        telefone = request.form.get('telefone')
+        email = request.form.get('email')
+        data_nascimento = request.form.get('data_nascimneto')
+
+
+        resultado = post_voluntario(nome, cpf, telefone, email, data_nascimento)
+
+        if resultado == 201:
+            flash('Cadastro bem-sucedido!', 'success')
+            return redirect(url_for('voluntario'))
+        else:
+            flash(f'erro: {resultado}', 'danger')
+            return redirect(url_for('cadastro_voluntario'))
+    return render_template('cadastro_voluntario.html')
+
+# listar ongs
+@app.route('/ongs')
+def listar_ongs():
+
+    ongs = get_ongs()
+
+    print(ongs)
+    return render_template("pagina_voluntario.html", ongs=ongs ['ongs'])
+
+# cadastrar ongs
+@app.route('/cadastro_ong', methods=['GET', 'POST'])
+def cadastro_ong():
+    if request.method == 'POST':
+        nome_ong = request.form.get('nome_ong')
+        chave_pix = request.form.get('chave_pix')
+        necessidades = request.form.get('necessidades')
+        imagem = request.form.get('imagem')
+        descricao = request.form.get('descricao')
+
+
+        resultado = post_ongs(nome_ong, chave_pix, necessidades, imagem, descricao)
+
+        if resultado == 201:
+            flash('Cadastro bem-sucedido!', 'success')
+            return redirect(url_for('voluntario'))
+        else:
+            flash(f'erro: {resultado}', 'danger')
+            return redirect(url_for('cadastro_ong'))
+    return render_template('casdastro_ong.html')
+
+#
+# # listar adocao
+# @app.route('/adocoes')
+# def listar_adocao():
+#
+#     adocoes = get_adocao()
+#
+#     print(adocao)
+#     return render_template("pagina_voluntario.html", ongs=ongs ['ongs'])
+#
+# # cadastrar adocao
+# @app.route('/cadastro_ong', methods=['GET', 'POST'])
+# def cadastro_ong():
+#     if request.method == 'POST':
+#         nome_ong = request.form.get('nome_ong')
+#         chave_pix = request.form.get('chave_pix')
+#         necessidades = request.form.get('necessidades')
+#         imagem = request.form.get('imagem')
+#         descricao = request.form.get('descricao')
+#
+#
+#         resultado = post_ongs(nome_ong, chave_pix, necessidades, imagem, descricao)
+#
+#         if resultado == 201:
+#             flash('Cadastro bem-sucedido!', 'success')
+#             return redirect(url_for('voluntario'))
+#         else:
+#             flash(f'erro: {resultado}', 'danger')
+#             return redirect(url_for('cadastro_ong'))
+#     return render_template('casdastro_ong.html')
+#
+
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=5001)
--- routes.py
+++ routes.py
@@ -1,16 +1,19 @@
+import pickle
+
 import requests
 
-base_url = "http://10.135.233.139:5000"
+base_url = "http://10.135.232.8:5000"
 
-def post_login(email, senha):
+def post_login(cpf, senha):
     try:
         url = f"{base_url}/login"
+        print(url)
         dados = {
-            "email": email,
+            "cpf": cpf,
             "senha": senha,
         }
         response = requests.post(url, json=dados)
-        return response.json()
+        return response
     except Exception as e:
         print(e)
         return {
@@ -30,51 +33,22 @@ def get_usuarios():
 
         }
 
-
-def get_usuarios():
+def post_usuario(nome, cpf, telefone, email, senha ):
     try:
-        url = f"{base_url}/listar_usuarios"
-        response = requests.get(url)
-        return response.json()
-    except Exception as e:
-        print(e)
-        return {
-            "error": f"{e}",
-
-        }
-def post_usuario(nome, cpf, email, senha, telefone):
-    try:
-        url = f"{base_url}/cadastro_usuario"
+        url = f"{base_url}/usuario/cadastrar"
         dados = {
             "nome": nome,
             "cpf": cpf,
+            "telefone": telefone,
             "email": email,
             "senha": senha,
-            "telefone": telefone,
         }
         response = requests.post(url, json=dados)
-        return response.json()
+        return response.status_code
     except Exception as e:
         print(e)
         return {
-            "error": f"{e}",
-        }
-
-def post_animal(nome, raca, idade, sexo):
-    try:
-        url = f"{base_url}/cadastro_animal"
-        dados = {
-            "nome": nome,
-            "raca": raca,
-            "idade": idade,
-            "sexo": sexo,
-        }
-        response = requests.post(url, json=dados)
-        return response.json()
-    except Exception as e:
-        print(e)
-        return {
-            "error": f"{e}",
+            "error": f"{e}"
         }
 
 # editar
@@ -84,9 +58,9 @@ def put_usuario(nome, cpf, email, senha, telefone,id_usuario):
         dados = {
             "nome": nome,
             "cpf": cpf,
+            "telefone": telefone,
             "email": email,
             "senha": senha,
-            "telefone": telefone,
         }
         response = requests.post(url, json=dados)
         return response.json()
@@ -97,9 +71,96 @@ def put_usuario(nome, cpf, email, senha, telefone,id_usuario):
         }
 
 
-def post_voluntario():
-    return None
+def get_animais():
+    try:
+        url = f"{base_url}/listar_animais"
+        response = requests.get(url)
+        return response.json()
+    except Exception as e:
+        print(e)
+        return {
+            "error": f"{e}",
+
+        }
+
+def post_animal(categoria, nome, raca, idade, sexo, imagem):
+    try:
+        url = f"{base_url}/cadastro/animal"
+        dados = {
+            "categoria": categoria,
+            "nome": nome,
+            "raca": raca,
+            "idade": idade,
+            "sexo": sexo,
+            "imagem": imagem,
+        }
+        response = requests.post(url, json=dados)
+        return response.status_code
+    except Exception as e:
+        print(e)
+        return {
+            "error": f"{e}",
+        }
 
 
-def post_ongs():
-    return None
+def get_voluntario():
+    try:
+        url = f"{base_url}/listar_voluntarios"
+        response = requests.get(url)
+        return response.json()
+    except Exception as e:
+        print(e)
+        return {
+            "error": f"{e}",
+
+        }
+
+def post_voluntario(nome, cpf, telefone, email, data_nascimneto ):
+    try:
+        url = f"{base_url}/cadastro_voluntario"
+        dados = {
+            "nome": nome,
+            "cpf": cpf,
+            "telefone": telefone,
+            "email": email,
+            "data_nascimneto": data_nascimneto,
+        }
+        response = requests.post(url, json=dados)
+        return response.status_code
+    except Exception as e:
+        print(e)
+        return {
+            "error": f"{e}"
+        }
+
+
+# ongs
+def get_ongs():
+    try:
+        url = f"{base_url}/listar_ongs"
+        response = requests.get(url)
+        return response.json()
+    except Exception as e:
+        print(e)
+        return {
+            "error": f"{e}",
+
+        }
+
+def post_ongs(nome_ong, chave_pix, necessidades, imagem, descricao ):
+    try:
+        url = f"{base_url}/cadastro_ong"
+        dados = {
+            "nome_ong": nome_ong,
+            "chave_pix": chave_pix,
+            "necessidades": necessidades,
+            "imagem": imagem,
+            "descricao": descricao,
+        }
+        response = requests.post(url, json=dados)
+        return response.status_code
+    except Exception as e:
+        print(e)
+        return {
+            "error": f"{e}"
+        }
